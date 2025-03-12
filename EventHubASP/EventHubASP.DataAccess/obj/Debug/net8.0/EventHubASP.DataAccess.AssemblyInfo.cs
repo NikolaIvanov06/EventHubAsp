454860a6616dff3f41b74dcda995f9d2283c93ef
NikolaIvanov06 <109094151+NikolaIvanov06@users.noreply.github.com>
2025-03-12 10:28:07 +0200

    Final Touches
    Final Touches

--- EventHubASP/EventHubASP.DataAccess/obj/Debug/net8.0/EventHubASP.DataAccess.AssemblyInfo.cs
+++ EventHubASP/EventHubASP.DataAccess/obj/Debug/net8.0/EventHubASP.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventHubASP.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b7668f7ef0ac5a8dab992bfea297177c3847ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a28e72023dd401f406aacd6c0e48e7408de40df")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventHubASP.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventHubASP.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
